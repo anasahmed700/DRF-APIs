c1032b1150867804d3664a8bf8da7784b1c5f6e7
Anas Ahmed <anasahmed700@gmail.com>
2025-05-04 04:09:05 +0500

    write a function based view to get products

--- api/views.py
+++ api/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
+from api.models import Product
+from api.serializers import ProductSerializer
+
+
+def product_list(request):
+    """
+    View to list all products.
+    """
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return JsonResponse({'data': serializer.data})
